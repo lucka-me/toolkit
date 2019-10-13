ef722f2bfca99983a400d3bb1df0624d87e886c6
lucka-me <samzhao0727@gmail.com>
2019-10-13 16:42:37 +0800

    Upload Ingress/Wayfarer-Dark-Mode [0.1.0]

---
+++ Ingress/Wayfarer-Dark-Mode/README.md
@@ -0,0 +1,17 @@
+# Dark Mode CSS for Niantic Wayfarer
+Enable dark mode for [Niantic Wayfarer review page](https://wayfarer.nianticlabs.com/review).
+
+## Usage
+1. Load CSS with a simple userscript: [Install](https://lucka.moe/toolkit/ingress/Wayfarer-Dark-Mode.user.js)
+2. Load CSS with userstyles manager like [Stylus](https://github.com/openstyles/stylus "Github")
+
+## References
+- [Dark Mode in CSS | CSS-Tricks](https://css-tricks.com/dark-modes-with-css/)  
+  How to enable dark mode with `prefers-color-scheme`
+- [Dark Theme - Material Design](https://material.io/design/color/dark-theme.html)
+
+## Changelog
+```markdown
+### [0.1.0] - 2019-10-13
+Initial version
+```
---
+++ Ingress/Wayfarer-Dark-Mode/Wayfarer-Dark-Mode.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Wayfarer Dark Mode
+// @namespace    http://lucka.moe/
+// @version      0.1.0
+// @author       lucka-me
+// @homepageURL  https://github.com/lucka-me/toolkit/tree/master/Ingress/Wayfarer-Dark-Mode
+// @updateURL    https://lucka.moe/toolkit/ingress/Wayfarer-Dark-Mode.user.js
+// @downloadURL  https://lucka.moe/toolkit/ingress/Wayfarer-Dark-Mode.user.js
+// @match        https://wayfarer.nianticlabs.com/review*
+// @run-at       document-start
+// ==/UserScript==
+
+const enableDarkMode = () => {
+    const link = document.createElement('link');
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    link.href = 'https://lucka.moe/toolkit/ingress/wayfarer-dark.css';
+    document.head.appendChild(link);
+};
+
+enableDarkMode();
