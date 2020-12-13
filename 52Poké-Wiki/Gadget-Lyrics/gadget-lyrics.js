6dfe7ba549adf4233f8adad96ce8c94e5c0d3cd9
lucka-me <samzhao0727@gmail.com>
2020-12-13 13:57:16 +0800

    Add comments

--- 52Poké-Wiki/Gadget-Lyrics/gadget-lyrics.js
+++ 52Poké-Wiki/Gadget-Lyrics/gadget-lyrics.js
@@ -2,6 +2,7 @@
     const lyricsBoxes = document.querySelectorAll('div.lyrics');
     if (lyricsBoxes.length < 1) return;
 
+    // Create a row with grid layout
     function createRow(name, gridTemplateColumns, items) {
         const row = document.createElement('div');
         if (name) row.className = name;
@@ -17,6 +18,7 @@
         return box
     };
 
+    // Split by <br> and trim every line
     function processLyricsText(text) {
         return text.split(/<br\/?>/).map(function (line) { return line.trim(); });
     };
@@ -25,24 +27,28 @@
         const lyricsBox = lyricsBoxes[indexLyricBox];
 
         const columns = Array.from(lyricsBox.querySelectorAll('.lyrics > div'));
+        // Collect data, title, className and splitted lyrics
         const datas = columns.map(function (column) {
-            
             return {
                 title: column.firstElementChild.innerHTML,
                 titleClassName: column.firstElementChild.className,
                 lyrics: processLyricsText(column.lastElementChild.querySelector('p').innerHTML)
             };
         });
+        // Find out maxnum of lyrics line count
         const lines = datas.reduce(function (prev, data) { return Math.max(prev, data.lyrics.length); }, 0);
         const gridTemplateColumns = lyricsBox.style.gridTemplateColumns;
         const contents = [];
+        // Generate rows
         for (var line = 0; line < lines; line++) {
             contents.push(createRow('', gridTemplateColumns, datas.map(function (data) {
                 const column = document.createElement('span');
-                column.innerHTML = data.lyrics.length < lines ? '\n' : data.lyrics[line];
+                // Keep an empty span if there are no more lines
+                column.innerHTML = data.lyrics.length < lines ? '' : data.lyrics[line];
                 return column;
             })));
         }
+        // Clear
         lyricsBox.innerHTML = '';
         lyricsBox.style.cssText = '';
         lyricsBox.append(createRow('lyrics-header', gridTemplateColumns, datas.map(function (data) {
